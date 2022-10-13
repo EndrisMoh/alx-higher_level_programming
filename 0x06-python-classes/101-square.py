ee8bc64189bfcde46fb3d55b4665cbaddae9b992
EndrisMoh <endrismd@gmail.com>
2022-10-13 11:44:30 -0700

    A class Square that defines printing square instance

--- 0x06-python-classes/101-square.py
+++ 0x06-python-classes/101-square.py
@@ -7,12 +7,7 @@ Defaults size to 0. Raise error on invalid size inputs.
 Attribute position which takes a default (0, 0) tuple.
 Methods Getter and Setter properties for size and position.
 Method area returns size of area of the square.
-Method my_print prints the square using "#", moved over left and top using
-position tuple.
-Method __repr__ should return the string to print out the square.
 """
-
-
 class Square:
     """A class that defines a square by size, which defaults 0.
     Also defines position using a tuple, which defaults (0, 0).
@@ -20,20 +15,32 @@ class Square:
     When printing, using position, offset on top and left.
     """
     def __init__(self, size=0, position=(0, 0)):
+        '''Initialization of instance attributes
+           size (int): Zero or positve number.
+        '''
         self.size = size
         self.position = position
 
+    def area(self):
+        '''Calculates the area
+            Return: The current square area.
+        '''
+        return self.__size * self.__size
+    
     @property
     def size(self):
         return self.__size
 
     @size.setter
-    def size(self, size):
-        if type(size) != int:
+    def size(self, value):
+        '''Updating the private attributes
+            value (int): Zero or positve number.
+        '''
+        if not isinstance(value, int):
             raise TypeError("size must be an integer")
-        if size < 0:
+        if value < 0:
             raise ValueError("size must be >= 0")
-        self.__size = size
+        self.__size = value
 
     @property
     def position(self):
@@ -41,30 +48,29 @@ class Square:
 
     @position.setter
     def position(self, value):
-        if type(value) != tuple or len(value) != 2 or \
-           not all([type(i) == int for i in value]):
-            raise TypeError("position must be a tuple of 2 positive integers")
-        self.__position = value
-
-    def __repr__(self):
-        return (self.get_str())
-
-    def area(self):
-        return self.__size * self.__size
-
-    def get_str(self):
-        total = ""
-        if self.__size == 0:
-            total += "\n"
-            return total
-        for i in range(self.__position[1]):
-            total += "\n"
-        for i in range(self.__size):
-            total += (" " * self.__position[0])
-            total += ("#" * self.__size)
-            if i is not (self.__size - 1):
-                total += "\n"
-        return total
-
+        '''Updating the private attributes
+           value (int): tuple of two positve numbers.
+        '''
+        if isinstance(value, tuple) and len(value) == 2:
+                if isinstance(value[0], int) and isinstance(value[1], int):
+                    if value[0] >= 0 and value[1] >= 0:
+                        self.__position = value
+                        return
+        raise TypeError("position must be a tuple of 2 positive integers")
+  
     def my_print(self):
-        print(self.get_str())
+        '''
+            prints in stdout the square with the character # or a new line
+            is size is zero.
+        '''
+        if self.__size == 0:
+            return ""
+
+        for line in range(self.__position[1]):
+                print()
+        for col in range(self.__size - 1):
+            print("{}{}".format(" " * self.__position[0], "#" * self.__size))
+        return "{}{}".format(" " * self.__position[0], "#" * self.__size)
+
+    def __str__(self):
+        return self.my_print()
