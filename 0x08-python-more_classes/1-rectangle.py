114d61d03f6e33286afe7806f801e14c078145c5
EndrisMoh <endrismd@gmail.com>
2022-10-17 13:06:01 -0700

    Real definition of a rectangle

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -1,30 +1,31 @@
 #!/usr/bin/python3
-'''Module 1
-    Defines a Rectangle
-'''
+"""Module 1-rectangle
+Defines a Rectangle class.
+"""
 
 
 class Rectangle:
+    """Rectangle class defined by width and height."""
     def __init__(self, width=0, height=0):
-        '''
-            Calling the setters to initialize the instance variables
-        '''
-        self.height = height
+        """Initializes a Rectangle instance.
+        Args:
+            width: width of the rectangle
+            height: height of the rectangle
+        """
         self.width = width
+        self.height = height
 
     @property
     def width(self):
-        '''
-            Returning the private var
-        '''
+        """Retrieves the width of a Rectangle instance."""
         return self.__width
 
     @width.setter
     def width(self, value):
-        '''
-            Checking for TypeError and ValueError
-            then setting up the private var
-        '''
+        """Sets the width of a Rectangle instance
+        Args:
+            value: value of the width, must be a positive integer
+        """
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
         if value < 0:
@@ -33,17 +34,15 @@ class Rectangle:
 
     @property
     def height(self):
-        '''
-            Returning the private var
-        '''
+        """Retrieves the height of a Rectangle instance."""
         return self.__height
 
     @height.setter
     def height(self, value):
-        '''
-            Checking for TypeError and ValueError
-            then setting up the private var
-        '''
+        """Sets the height of a Rectangle instance
+        Args:
+            value: value of the height, must be a positive integer
+        """
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
         if value < 0:
