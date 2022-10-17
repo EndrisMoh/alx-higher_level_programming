cb90063629b8ce44c2e801ff1bad456029e92e38
EndrisMoh <endrismd@gmail.com>
2022-10-17 13:51:22 -0700

    Compare rectangles

--- 0x08-python-more_classes/8-rectangle.py
+++ 0x08-python-more_classes/8-rectangle.py
@@ -42,6 +42,7 @@ class Rectangle:
         that is able to recreate a new instance by using eval()
         """
         return "Rectangle({}, {})".format(self.__width, self.__height)
+
     def __del__(self):
         """Deletes a Rectangle instance."""
         print("Bye rectangle...")
