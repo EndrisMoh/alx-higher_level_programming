0ae304f868da20508e7e91e06296ab96e9d8f9e2
EndrisMoh <endrismd@gmail.com>
2022-10-25 12:08:30 -0700

    A class to JSON that returns the dictionary description

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 """Module 8-class_to_json.
-Returns the dictionary description with simple data structure (list, dictionary,
-string, integer and boolean) for JSON serialization of an object.
+Returns the dictionary description with simple data structure
+(list, dictionary,string, integer and boolean) for JSON serialization
+of an object.
 """
 
 
