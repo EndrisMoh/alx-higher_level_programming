636b63152e0a421e3ba20bdc19fc4c2c61c5928f
EndrisMoh <endrismd@gmail.com>
2022-12-24 12:44:57 -0800

    A script that lists all cities from the database hbtn_0e_4_usa

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+A script that lists all cities of the database hbtn_0e_4_usa.
+It connect to a MySQL server running on localhost at port 3306
+Results must be sorted in ascending order by cities.id
+You can use only execute() once
+"""
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    c = db.cursor()
+    c.execute("SELECT `c`.`id`, `c`.`name`, `s`.`name` \
+                 FROM `cities` as `c` \
+                INNER JOIN `states` as `s` \
+                   ON `c`.`state_id` = `s`.`id` \
+                ORDER BY `c`.`id`")
+    [print(city) for city in c.fetchall()]
