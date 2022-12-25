8e6e3f464073fa7cd623483f348e45d6678769af
EndrisMoh <endrismd@gmail.com>
2022-12-25 12:37:17 -0800

    A script that lists all State objects, and corresponding City objects, contained in the database

---
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""
+Lists all State objects and corresponding City objects contained in the DB
+You must use the module SQLAlchemy & only use one query to the database
+You must use the cities relationship for all State objects
+Results must be sorted in ascending order by states.id and cities.id
+Results must be displayed as
+      <state id>: <state name>
+      <tabulation><city id>: <city name>
+"""
+import sys
+from relationship_state import Base, State
+from relationship_city import City
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+
+if __name__ == '__main__':
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.
+                           format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    st = session.query(State).outerjoin(City).order_by(State.id, City.id).all()
+
+    for state in st:
+        print("{}: {}".format(state.id, state.name))
+        for city in state.cities:
+            print("    {}: {}".format(city.id, city.name))
