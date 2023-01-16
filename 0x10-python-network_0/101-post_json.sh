bc00c743169940e9f2aa1f545a78305e9d621f01
EndrisMoh <endrismd@gmail.com>
2023-01-16 02:58:45 -0800

    cURL a JSON file

---
+++ 0x10-python-network_0/101-post_json.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# A Bash script that sends a JSON `POST` request to a URL passed as the first argument, and displays the body of the response
+curl -sX POST -H "Content-type: application/json" -H "Accept: application/json" -d "@$2" "$1"
