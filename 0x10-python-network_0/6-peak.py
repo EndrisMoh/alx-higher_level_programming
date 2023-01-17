8837f8d1cee889ed57324a41e183b89cfad0344b
EndrisMoh <endrismd@gmail.com>
2023-01-17 12:31:53 -0800

    Find a peak of integers

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -3,22 +3,30 @@
     a list of unsorted integers
 """
 
-"""
-    Algorithm Complexity Process
-        it is not sorted, so sorting would take n(log(n))
-            -> not worth sorting
-        looping through and keeping track of max (brute force)
-            -> O(n)
-        possibly looping from each end reducing to 1/2 run time
-            -> still O(n)
-"""
-
 
 def find_peak(list_of_integers):
-    """BRUTE force implementation for question
+       """
+    Args:
+        list_of_integers(int): list of integers to find peak of
+    Returns: peak of list_of_integers or None
     """
-    max_int = None
-    for n in list_of_integers:
-        if max_int is None or max_int < n:
-            max_int = n
-    return max_int
+    size = len(list_of_integers)
+    mid_e = size
+    mid = size // 2
+
+    if size == 0:
+        return None
+
+    while True:
+        mid_e = mid_e // 2
+        if (mid < size - 1 and
+                list_of_integers[mid] < list_of_integers[mid + 1]):
+            if mid_e // 2 == 0:
+                mid_e = 2
+            mid = mid + mid_e // 2
+        elif mid_e > 0 and list_of_integers[mid] < list_of_integers[mid - 1]:
+            if mid_e // 2 == 0:
+                mid_e = 2
+            mid = mid - mid_e // 2
+        else:
+	        return list_of_integers[mid]
